c5f627b0044047bee51e68a03b071d691c612515
blakboy <44412275+blakboy@users.noreply.github.com>
2018-11-27 18:58:36 -0300

    Add files via upload

---
+++ MisPerris_EN2/Sistema/static/js/codigo.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".carrusel").bxSlider({
+        slideWidth:1000,
+        pauseOnAction:false,
+        adaptiveHeight:true,
+        made:"vertical"
+    }
+        
+    );
+  
+    
+})
